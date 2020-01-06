80322e7b92230f047f7a3cad6570988a67cde6ab
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-01-06 17:26:50 -0600

    "html bone structure as well as storage

---
+++ script.js
@@ -0,0 +1,12 @@
+//PUESDOCODE FOR WEATHER DASHBORED
+
+//WEATHER API $AJAX
+// apikey: 69f15c00682d18316696a7296341bf67
+
+//SEARCH BUTTON FOR CITY
+    // SAVE IN LOCAL STOARGE
+    localStorage.setItem("key", "value");
+    // POP UP 5 DAY FORECAST IN ITS OWN DIV
+    // 
+    
+    
