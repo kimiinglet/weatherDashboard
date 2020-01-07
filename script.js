5fa7e5386d60395e086a2f7b656fe28b2989f9fb
Maggie Kiefer <57575667+magsmagsmags@users.noreply.github.com>
2020-01-07 09:39:21 -0600

    local storage, states

--- script.js
+++ script.js
@@ -1,12 +1,49 @@
 //PUESDOCODE FOR WEATHER DASHBORED
 
 //WEATHER API $AJAX
-// apikey: 69f15c00682d18316696a7296341bf67
+var apiKey = '69f15c00682d18316696a7296341bf67';
+var units = 'imperial';
+var searchMethod ='zip';
+
+// function searchWeather(searchTerm) {
+//     fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appID}&units=${units}`).then(result => {
+//         return result.json();
+//     }).then(result => {
+//         init(result);
+//     })
+// }
+//  function init(resultFromServer){
+//      console.log(resultFromServer);
+//  }
+
+//  document.getElementById('searchBtn').addEventListener('click', () => {
+//      var searchTerm = document.getElementById('searchInput').nodeValue;
+//     if(searchTerm) {
+//         searchWeather(searchTerm);
+//     };
+//  })
 
 //SEARCH BUTTON FOR CITY
     // SAVE IN LOCAL STOARGE
-    localStorage.setItem("key", "value");
+
     // POP UP 5 DAY FORECAST IN ITS OWN DIV
     // 
-    
+   
+// Get location 
+getLocation();
+
+function getLocation(){
+    if( navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        alert("Location in not supported by the browser!");
+    }
+}
+
+function getWeather() {
+    $(document).ready(function(){
+        $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=")
+    })
+}
     
