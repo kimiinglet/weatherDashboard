a36b15a03c4d388e1d81ba44e3d8d3881c8458b6
kimiinglet <kinglet91391@gmail.com>
2020-02-05 18:40:40 -0600

    ajax calls completed

--- script.js
+++ script.js
@@ -1,74 +1,75 @@
 //API KEY
 var APIKey = "69f15c00682d18316696a7296341bf67";
-// var city = $("#search_i");
 
-//URL SEARCH CITY
-
-//variables 
-
-// var tempEl = $("#temp_l");
-// var humidityEl = $("#humidity_l");
-// var windEl = $("#wind_speed_l");
-// var uvEl = $("#uv_index_l");
+var humidity = document.getElementById("humidity");
+var windSpeed = document.getElementById("windSpeed");
+var uvIndex = document.getElementById("uvIndex");
 
 
-// // Todays date;
-// var today = new Date();
-// var dd = String(today.getDate()).padStart(2, '0');
-// var mm = String(today.getMonth() + 1).padStart(2, '0'); //Jan = 0!
-// var yyyy = today.getYear();
 
-// today = mm + '/' + dd + '/' + yyyy;
-// document.write(today);
+// Todays date;
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0'); //Jan = 0!
+var yyyy = today.getYear();
+
+today = mm + '/' + dd + '/' + yyyy;
+document.write(today);
 
 
-// API url
 
 
 //search button
 var searchBtn = $("#searchB");
 // let searchBtn = document.getElementById("searchB")
+
+
+
+
+
+//when the document loads ->
 $(document).ready(function(){
+
+    //listening for a click on search button
     searchBtn.on("click", function(e){
         e.preventDefault()
-        //way to get it input
-         let city = e.currentTarget.previousElementSibling.value
-        //  var queryURL = "https://api.openweathermap.org/data/2.5/weather?" + city + "&units=imperial&appid=" + APIKey;
+
+        //grab input from html element
+        let city = e.currentTarget.previousElementSibling.value
+
+        // construct a url for api from the information we got from the html input
         let queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`
-        console.log(city)
-          // Performing our AJAX GET request
+ 
+
+          // Perform our AJAX GET request with our url
           $.ajax({
             url: queryURL,
             method: "GET"
           })
             // After the data comes back from the API
-            .then(function(response) {
-                console.log(response.weather)
-                let div = document.getElementById("temp")
-                div.innerHTML = response.weather[0].main
-                
-              // Storing an array of results in the results variable
-            //   var results = response.data;
-    
-    })
+            .then(function(data) {
+                console.log(data)
+              humidity.innerHTML = data.main.humidity
+              windSpeed.innerHTML = data.wind.speed 
+              
+              let long = data.coord.lon
+              let lat = data.coord.lat
+
+            let uvIndexURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${long}&appid=${APIKey}`
+
+            //Perform AJAX for uvIndex
+              $.ajax({
+                url: uvIndexURL,
+                method: "GET"
+              })
+              .then(function(data) {
+                uvIndex.innerHTML = data.value
+              }
+            )
 })
 })
+})
 
 
-//way to get it input
-// response.currentTarget.previousElementSibling.value
 
-
-//city buttons to the city forecast
-
-
-//day 1: Date/ Icon / Temp/ Humidity
-
-//day 2: Date/ Icon / Temp/ Humidity
-
-//day 3: Date/ Icon / Temp/ Humidity
-
-//day 4: Date/ Icon / Temp/ Humidity
-
-//day 5: Date/ Icon / Temp/ Humidity
 
